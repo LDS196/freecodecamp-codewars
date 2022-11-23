dcb472f346fe9373162e46b0f5d6061123f21ed1
LDS196 <lihachevd87@gmail.com>
2022-11-23 17:33:10 +0400

    new

--- codewars/01.js
+++ codewars/01.js
@@ -22,17 +22,37 @@
 //  let b = a.flat()
 // return b
 // }
-s = "aeiou, abc" 
-function vowelOne(s){
-  let b = []
-  let a = s.split('').filter(letter => {
-    if( ['a','e','i','o','u',].includes(letter.toLowerCase())){
-      b.push(1)
-    } else {
-      b.push(0)
-    }
-  })
-  return b.join('')
+// s = "aeiou, abc" 
+// function vowelOne(s){
+//   let b = []
+//   let a = s.split('').filter(letter => {
+//     if( ['a','e','i','o','u',].includes(letter.toLowerCase())){
+//       b.push(1)
+//     } else {
+//       b.push(0)
+//     }
+//   })
+//   return b.join('')
 
+// }
+const suspectInfo = {
+'James': ['Jacob', 'Bill', 'Lucas'],
+'Johnny': ['David', 'Kyle', 'Lucas'],
+'Peter': ['Lucy', 'Kyle']
+};
+const dead = ['Lucas', 'Bill']
+
+function killer(suspectInfo, dead) {
+  let arr = []
+  for(let key in suspectInfo){ 
+    let count = 0;
+    for( let i = 0; i < dead.length; i++){
+      if(suspectInfo[key].includes(dead[i])){
+        count = count + 1;
+      }
+    }
+    arr.push(count)
+  }
+  return Object.keys(suspectInfo)[arr.indexOf(Math.max(...arr))]
 }
 
