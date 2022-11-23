6ae6175b2339a62b66552a9b79a636812fd640e6
LDS196 <lihachevd87@gmail.com>
2022-11-23 16:09:14 +0400

    new

--- codewars/01.js
+++ codewars/01.js
@@ -5,12 +5,20 @@
 // let odd = S.split('').filter((elem, i) => i % 2 !== 0).join('');
 //  return even + ' ' + odd;
 // }
-function func (...a){
-
-}
 
 
 function spread(func, args) {
   return func(...args)
 
+}
+
+
+ function dataReverse(data) {
+
+  let a = [];
+  for( let i = 0; i < data.length; i = i + 8){
+   a.unshift(data.slice(i , i + 8))
+ }
+ let b = a.flat()
+return b
 }
