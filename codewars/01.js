631327fb8311836002ae095a960e5fb6e2a3872d
LDS196 <lihachevd87@gmail.com>
2022-11-23 12:06:16 +0400

    new

--- codewars/01.js
+++ codewars/01.js
@@ -1,10 +1,16 @@
-(("CodeWars"), "CdWr oeas");
-const S = 'CodeWars'
 
-function sortMyString(S) {
-let even = S.split('').filter((elem, i) => i % 2 == 0).join('');
-let odd = S.split('').filter((elem, i) => i % 2 !== 0).join('');
- return even + ' ' + odd;
+// const S = 'CodeWars'
+// function sortMyString(S) {
+// let even = S.split('').filter((elem, i) => i % 2 == 0).join('');
+// let odd = S.split('').filter((elem, i) => i % 2 !== 0).join('');
+//  return even + ' ' + odd;
+// }
+function func (...a){
+
 }
 
-console.log(sortMyString(S))
+
+function spread(func, args) {
+  return func(...args)
+
+}
