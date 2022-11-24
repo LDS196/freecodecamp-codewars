d71d130abaa9a30e671f031fa431b0147c83454a
LDS196 <lihachevd87@gmail.com>
2022-11-24 16:08:03 +0400

    new

--- codewars/01.js
+++ codewars/01.js
@@ -121,57 +121,68 @@
 //   return name.slice(0,4)
 //  } else return name.slice(0,3)
 // }
-const first = {
-  'algebra': 6,
-  'history': 7,
-  'physics': 8,
-  'geography': 9,
-  'chemistry': 10
-};
-const second = {
-  'algebra': 8,
-  'history': 7,
-  'physics': 8,
-  'geography': 9,
-  'chemistry': 10
-};
-const third = {
-  'algebra': 6,
-  'history': 5,
-  'physics': 5,
-  'geography': 9,
-  'chemistry': 10
-};
+// const first = {
+//   'algebra': 6,
+//   'history': 7,
+//   'physics': 8,
+//   'geography': 9,
+//   'chemistry': 10
+// };
+// const second = {
+//   'algebra': 8,
+//   'history': 7,
+//   'physics': 8,
+//   'geography': 9,
+//   'chemistry': 10
+// };
+// const third = {
+//   'algebra': 6,
+//   'history': 5,
+//   'physics': 5,
+//   'geography': 9,
+//   'chemistry': 10
+// };
 
-function whoseBicycle(diary1, diary2, diary3) {
- let sum1 = 0;
- let sum2 = 0;
- let sum3 = 0;
- for(let key in diary2){
-  sum2 = sum2 + diary2[key]
- }
- for(let key in diary1){
-  sum1 = sum1 + diary1[key]
- }
- for(let key in diary3){
-  sum3 = sum3 + diary3[key]
- }
- //console.log(sum1,sum2, sum3)
- if( sum1 == sum2 && sum2 == sum3){
-  return 'I need to buy a bicycle for my third son.' //  the sum of the marks is the highest in the third diary.
- } else if(sum1 == sum2 && sum1 > sum3 && sum2 > sum3){
-  return 'I need to buy a bicycle for my second son.'
- } else if(sum1 == sum3 && sum1 > sum2 && sum3 > sum2){
-  return 'I need to buy a bicycle for my third son.' //  the sum of the marks is the highest in the third diary.
- } else if(sum2 == sum3 && sum2 > sum1 && sum3 > sum1){
-  return 'I need to buy a bicycle for my third son.' //  the sum of the marks is the highest in the third diary.
- } else if(sum1 > sum2 && sum1 > sum3 ){
-  return 'I need to buy a bicycle for my first son.'
- } else if(sum2 > sum1 && sum2 > sum3 ){
-  return 'I need to buy a bicycle for my second son.' 
- } else if(sum3 > sum1 && sum3 > sum2 ){
-  return 'I need to buy a bicycle for my third son.'
- } 
+// function whoseBicycle(diary1, diary2, diary3) {
+//  let sum1 = 0;
+//  let sum2 = 0;
+//  let sum3 = 0;
+//  for(let key in diary2){
+//   sum2 = sum2 + diary2[key]
+//  }
+//  for(let key in diary1){
+//   sum1 = sum1 + diary1[key]
+//  }
+//  for(let key in diary3){
+//   sum3 = sum3 + diary3[key]
+//  }
+//  //console.log(sum1,sum2, sum3)
+//  if( sum1 == sum2 && sum2 == sum3){
+//   return 'I need to buy a bicycle for my third son.' //  the sum of the marks is the highest in the third diary.
+//  } else if(sum1 == sum2 && sum1 > sum3 && sum2 > sum3){
+//   return 'I need to buy a bicycle for my second son.'
+//  } else if(sum1 == sum3 && sum1 > sum2 && sum3 > sum2){
+//   return 'I need to buy a bicycle for my third son.' //  the sum of the marks is the highest in the third diary.
+//  } else if(sum2 == sum3 && sum2 > sum1 && sum3 > sum1){
+//   return 'I need to buy a bicycle for my third son.' //  the sum of the marks is the highest in the third diary.
+//  } else if(sum1 > sum2 && sum1 > sum3 ){
+//   return 'I need to buy a bicycle for my first son.'
+//  } else if(sum2 > sum1 && sum2 > sum3 ){
+//   return 'I need to buy a bicycle for my second son.' 
+//  } else if(sum3 > sum1 && sum3 > sum2 ){
+//   return 'I need to buy a bicycle for my third son.'
+//  } 
+// }
+
+let a = [-13,-50,57,13,67,-13,57,108,67]
+function maxTriSum(numbers){
+  let b = []
+  let a = numbers.sort((a, b) => b - a)//.slice(0,3).reduce((acc, value) => acc + value, 0)
+  for(let i = 0; i < a.length; i++){
+    if( !b.includes(a[i])){
+      b.push(a[i])
+    } else continue
+  }
+  return b.slice(0,3).reduce((acc, value) => acc + value, 0)
 }
-
-console.log(whoseBicycle(first, second, third))
+console.log( maxTriSum(a))
